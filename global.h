874129906bfbab4798f7557fa074c780d3ba81e2
guol <guolin2509@gmail.com>
2022-05-27 20:10:39 +0800

    add euler para

--- global.h
+++ global.h
@@ -1,7 +1,14 @@
 // Here, global variables are defined
 
-const float k = 1.0;
+const double k = 1.0;
 const int N = 100;
-const float Tend = 2.0;
+const double Tend = 2.0;
 const int nsteps = 5000;
 const int L = 1;
+
+
+// coefs still tbd
+const double g = 1.0;
+const double h = 1.0;
+const double c = 1.0;
+const double rho = 1.0;
