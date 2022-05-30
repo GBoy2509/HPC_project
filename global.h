caceb1fb89a09bd23a90190217229a2840f8c557
guol <guolin2509@gmail.com>
2022-05-30 15:06:39 +0800

    N=100,nsteps=5000,have tested

--- global.h
+++ global.h
@@ -1,14 +1,14 @@
 // Here, global variables are defined
 
-const double k = 1.0;
-const int N = 100;
-const double Tend = 2.0;
-const int nsteps = 5000;
-const int L = 1;
+double kcond = 1.0;
+int N = 100;
+double Tend = 2.0;
+int nsteps = 5000;
+double L = 1.0;
 
 
 // coefs still tbd
-const double g = 1.0;
-const double h = 1.0;
-const double c = 1.0;
-const double rho = 1.0;
+double g = 1.0;
+double h = 1.0;
+double c = 1.0;
+double rho = 1.0;
